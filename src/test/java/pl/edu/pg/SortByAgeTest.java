8a7684ecc1888d17aa35c31b48a0da5bb7d8cdc8
VV01T3K <wojteks.access@gmail.com>
2025-03-10 22:08:11 +0100

    Changed names of instances of comparator in tests

--- src/test/java/pl/edu/pg/SortByAgeTest.java
+++ src/test/java/pl/edu/pg/SortByAgeTest.java
@@ -8,38 +8,44 @@ public class SortByAgeTest {
 
     @Test
     void compareWithNull() {
-        SortByAge sortByAge = new SortByAge();
+
+        SortByAge comparator = new SortByAge();
 
         Czlowiek A = new Czlowiek("A", "A", 20);
 
-        assertEquals(0, sortByAge.compare(null, null)); // null === null
+        assertEquals(0, comparator.compare(null, null)); // null === null
 
-        assertEquals(-1, sortByAge.compare(null, A)); // null == o1
+        assertEquals(-1, comparator.compare(null, A)); // null == o1
 
-        assertEquals(1, sortByAge.compare(A, null)); // o2 == null
+        assertEquals(1, comparator.compare(A, null)); // o2 == null
+
     }
 
     @Test
     void compareWithSameAge() {
-        SortByAge sortByAge = new SortByAge();
+
+        SortByAge comparator = new SortByAge();
 
         Czlowiek A = new Czlowiek("A", "A", 20);
         Czlowiek B = new Czlowiek("B", "B", 20);
 
-        assertEquals(0, sortByAge.compare(A, B)); // o1 == o2
-        assertEquals(0, sortByAge.compare(A, A));
-        assertEquals(0, sortByAge.compare(B, B));
+        assertEquals(0, comparator.compare(A, B)); // o1 == o2
+        assertEquals(0, comparator.compare(A, A));
+        assertEquals(0, comparator.compare(B, B));
+
     }
 
     @Test
     void compareWithDifferentAge() {
-        SortByAge sortByAge = new SortByAge();
+
+        SortByAge comparator = new SortByAge();
 
         Czlowiek A_old = new Czlowiek("A", "A", 20);
         Czlowiek B_young = new Czlowiek("B", "B", 19);
 
-        assertEquals(1, sortByAge.compare(A_old, B_young)); // o1 > o2
+        assertEquals(1, comparator.compare(A_old, B_young)); // o1 > o2
 
-        assertEquals(-1, sortByAge.compare(B_young, A_old)); // o1 < o2
+        assertEquals(-1, comparator.compare(B_young, A_old)); // o1 < o2
+
     }
 }
