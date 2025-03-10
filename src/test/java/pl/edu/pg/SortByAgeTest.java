c6722d843050bc37a9b6a8e0f8da08ac86079153
Wojciech Siwiec <s197815@student.pg.edu.pl>
2025-03-10 13:33:28 +0000

    Dodano intefejs SortByAge ( comparator )

--- src/test/java/pl/edu/pg/SortByAgeTest.java
+++ src/test/java/pl/edu/pg/SortByAgeTest.java
@@ -8,64 +8,64 @@ import java.util.TreeSet;
 
 public class SortByAgeTest {
 
-    @Test
-    void compareWithNull() {
+        @Test
+        void compareWithNull() {
 
-        Czlowiek A = new Czlowiek("A", "A", 20, Plec.MEZCZYZNA);
+                Czlowiek A = new Czlowiek("A", "A", 20, Plec.MEZCZYZNA);
 
-        SortByAge comparator = new SortByAge();
+                SortByAge comparator = new SortByAge();
 
-        assertThrows(NullPointerException.class, () -> comparator.compare(null, null));
-        assertThrows(NullPointerException.class, () -> comparator.compare(null, A));
-        assertThrows(NullPointerException.class, () -> comparator.compare(A, null));
+                assertThrows(NullPointerException.class, () -> comparator.compare(null, null));
+                assertThrows(NullPointerException.class, () -> comparator.compare(null, A));
+                assertThrows(NullPointerException.class, () -> comparator.compare(A, null));
 
-    }
+        }
 
-    @Test
-    void compareWithSameAge() {
+        @Test
+        void compareWithSameAge() {
 
-        SortByAge comparator = new SortByAge();
+                SortByAge comparator = new SortByAge();
 
-        Czlowiek A = new Czlowiek("A", "A", 20, Plec.MEZCZYZNA);
-        Czlowiek AA = new Czlowiek("A", "A", 20, Plec.MEZCZYZNA);
-        Czlowiek B = new Czlowiek("B", "B", 20, Plec.MEZCZYZNA);
-        Czlowiek BB = new Czlowiek("B", "B", 20, Plec.MEZCZYZNA);
+                Czlowiek A = new Czlowiek("A", "A", 20, Plec.MEZCZYZNA);
+                Czlowiek AA = new Czlowiek("A", "A", 20, Plec.MEZCZYZNA);
+                Czlowiek B = new Czlowiek("B", "B", 20, Plec.MEZCZYZNA);
+                Czlowiek BB = new Czlowiek("B", "B", 20, Plec.MEZCZYZNA);
 
-        assertEquals(-1, comparator.compare(A, B)); // o1 < o2
-        assertEquals(0, comparator.compare(A, AA)); // o1 == o2
-        assertEquals(0, comparator.compare(B, BB)); // o1 == o2
+                assertEquals(-1, comparator.compare(A, B)); // o1 < o2
+                assertEquals(0, comparator.compare(A, AA)); // o1 == o2
+                assertEquals(0, comparator.compare(B, BB)); // o1 == o2
 
-    }
+        }
 
-    @Test
-    void compareWithDifferentAge() {
+        @Test
+        void compareWithDifferentAge() {
 
-        SortByAge comparator = new SortByAge();
+                SortByAge comparator = new SortByAge();
 
-        Czlowiek A = new Czlowiek("A", "A", 69, Plec.MEZCZYZNA);
-        Czlowiek B = new Czlowiek("B", "B", 42, Plec.MEZCZYZNA);
+                Czlowiek A = new Czlowiek("A", "A", 69, Plec.MEZCZYZNA);
+                Czlowiek B = new Czlowiek("B", "B", 42, Plec.MEZCZYZNA);
 
-        assertEquals(1, comparator.compare(A, B)); // o1 > o2
+                assertEquals(1, comparator.compare(A, B)); // o1 > o2
 
-        assertEquals(-1, comparator.compare(B, A)); // o1 < o2
+                assertEquals(-1, comparator.compare(B, A)); // o1 < o2
 
-    }
+        }
 
-    @Test
-    void treeSetTest() {
+        @Test
+        void treeSetTest() {
 
-        Czlowiek A = new Czlowiek("A", "A", 42, Plec.MEZCZYZNA);
-        Czlowiek B = new Czlowiek("B", "B", 69, Plec.MEZCZYZNA);
-        Czlowiek C = new Czlowiek("C", "C", 21, Plec.MEZCZYZNA);
+                Czlowiek A = new Czlowiek("A", "A", 42, Plec.MEZCZYZNA);
+                Czlowiek B = new Czlowiek("B", "B", 69, Plec.MEZCZYZNA);
+                Czlowiek C = new Czlowiek("C", "C", 21, Plec.MEZCZYZNA);
 
-        TreeSet<Czlowiek> set = new TreeSet<>(new SortByAge());
-        set.add(A);
-        set.add(B);
-        set.add(C);
-        assertEquals(3, set.size());
+                TreeSet<Czlowiek> set = new TreeSet<>(new SortByAge());
+                set.add(A);
+                set.add(B);
+                set.add(C);
+                assertEquals(3, set.size());
 
-        assertEquals(C, set.first());
-        assertEquals(B, set.last());
+                assertEquals(C, set.first());
+                assertEquals(B, set.last());
 
-    }
+        }
 }
