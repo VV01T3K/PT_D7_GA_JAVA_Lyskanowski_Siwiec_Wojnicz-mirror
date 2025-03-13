6a5399d530723236fc1c1c59d6e69b2f10667e98
VV01T3K <wojteks.access@gmail.com>
2025-03-13 22:51:46 +0000

    changed dzieciPowinnyBycNiesortowane to actually test intended behavior before it was trying to match unorderd set to orderd list

--- src/test/java/pl/edu/pg/CzlowiekDzieciFactoryTest.java
+++ src/test/java/pl/edu/pg/CzlowiekDzieciFactoryTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CzlowiekDzieciFactoryTest {
@@ -13,11 +14,11 @@ class CzlowiekDzieciFactoryTest {
   @Test
   void dzieciPowinnyBycPrawidlowoSortowaneNaturalnie() {
     CzlowiekDzieciFactory.setSortMode(SortModes.ORDERED);
-    Czlowiek dziecko1 = new Czlowiek("Jan", "Kowalski", 20,Plec.MEZCZYZNA);
-    Czlowiek dziecko2 = new Czlowiek("Jan", "Nowak", 21,Plec.MEZCZYZNA);
-    Czlowiek dziecko3 = new Czlowiek("Marek", "Kowalski", 21,Plec.MEZCZYZNA);
-    Czlowiek dziecko4 = new Czlowiek("Marek", "Nowak", 21,Plec.MEZCZYZNA);
-    Czlowiek rodzic = new Czlowiek("Jan", "Kowalski", 20,Plec.MEZCZYZNA);
+    Czlowiek dziecko1 = new Czlowiek("Jan", "Kowalski", 20, Plec.MEZCZYZNA);
+    Czlowiek dziecko2 = new Czlowiek("Jan", "Nowak", 21, Plec.MEZCZYZNA);
+    Czlowiek dziecko3 = new Czlowiek("Marek", "Kowalski", 21, Plec.MEZCZYZNA);
+    Czlowiek dziecko4 = new Czlowiek("Marek", "Nowak", 21, Plec.MEZCZYZNA);
+    Czlowiek rodzic = new Czlowiek("Jan", "Kowalski", 20, Plec.MEZCZYZNA);
     rodzic.dodajDziecko(dziecko1);
     rodzic.dodajDziecko(dziecko2);
     rodzic.dodajDziecko(dziecko3);
@@ -29,11 +30,11 @@ class CzlowiekDzieciFactoryTest {
   void dzieciPowinnyBycPrawidlowoSortowaneZgodnieZKomparator() {
     CzlowiekDzieciFactory.setSortMode(SortModes.ORDERED);
     CzlowiekDzieciFactory.setComparator(Comparator.comparing(Czlowiek::getWiek));
-    Czlowiek dziecko1 = new Czlowiek("Jan", "Kowalski", 20,Plec.MEZCZYZNA);
-    Czlowiek dziecko2 = new Czlowiek("Jan", "Nowak", 19,Plec.MEZCZYZNA);
-    Czlowiek dziecko3 = new Czlowiek("Marek", "Kowalski", 18,Plec.MEZCZYZNA);
-    Czlowiek dziecko4 = new Czlowiek("Marek", "Nowak", 21,Plec.MEZCZYZNA);
-    Czlowiek rodzic = new Czlowiek("Jan", "Kowalski", 50,Plec.MEZCZYZNA);
+    Czlowiek dziecko1 = new Czlowiek("Jan", "Kowalski", 20, Plec.MEZCZYZNA);
+    Czlowiek dziecko2 = new Czlowiek("Jan", "Nowak", 19, Plec.MEZCZYZNA);
+    Czlowiek dziecko3 = new Czlowiek("Marek", "Kowalski", 18, Plec.MEZCZYZNA);
+    Czlowiek dziecko4 = new Czlowiek("Marek", "Nowak", 21, Plec.MEZCZYZNA);
+    Czlowiek rodzic = new Czlowiek("Jan", "Kowalski", 50, Plec.MEZCZYZNA);
     rodzic.dodajDziecko(dziecko1);
     rodzic.dodajDziecko(dziecko2);
     rodzic.dodajDziecko(dziecko3);
@@ -44,13 +45,16 @@ class CzlowiekDzieciFactoryTest {
   @Test
   void dzieciPowinnyBycNiesortowane() {
     CzlowiekDzieciFactory.setSortMode(SortModes.UNORDERED);
-    Czlowiek dziecko1 = new Czlowiek("Jan", "Kowalski", 20,Plec.MEZCZYZNA);
-    Czlowiek dziecko2 = new Czlowiek("Jan", "Nowak", 21,Plec.MEZCZYZNA);
-    Czlowiek dziecko3 = new Czlowiek("Marek", "Kowalski", 21,Plec.MEZCZYZNA);
-    Czlowiek rodzic = new Czlowiek("Marek", "Nowak", 21,Plec.MEZCZYZNA);
+    Czlowiek dziecko1 = new Czlowiek("Jan", "Kowalski", 20, Plec.MEZCZYZNA);
+    Czlowiek dziecko2 = new Czlowiek("Jan", "Nowak", 21, Plec.MEZCZYZNA);
+    Czlowiek dziecko3 = new Czlowiek("Marek", "Kowalski", 21, Plec.MEZCZYZNA);
+    Czlowiek rodzic = new Czlowiek("Marek", "Nowak", 21, Plec.MEZCZYZNA);
     rodzic.dodajDziecko(dziecko1);
     rodzic.dodajDziecko(dziecko2);
     rodzic.dodajDziecko(dziecko3);
-    assertEquals(List.of(dziecko1, dziecko3, dziecko2), new ArrayList<>(rodzic.getDzieci()));
+    assertAll(
+        () -> assertEquals(true, rodzic.getDzieci().contains(dziecko1)),
+        () -> assertEquals(true, rodzic.getDzieci().contains(dziecko2)),
+        () -> assertEquals(true, rodzic.getDzieci().contains(dziecko3)));
   }
 }
