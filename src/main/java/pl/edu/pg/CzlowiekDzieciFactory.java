c5104ffe88f2f6c9d9dcac6aa7e893809f5b0be9
Bartosz <bartilyska@gmail.com>
2025-03-14 02:22:19 +0100

    refactored wypelnijDzieci function

--- src/main/java/pl/edu/pg/CzlowiekDzieciFactory.java
+++ src/main/java/pl/edu/pg/CzlowiekDzieciFactory.java
@@ -27,6 +27,24 @@ public class CzlowiekDzieciFactory {
       return new HashSet<>();
     }
   }
+  private static void randomizuj(ArrayList<Czlowiek> mezczyzni, ArrayList<Czlowiek> kobiety, Stack<Czlowiek> potomstwo) {
+    Random rand = new Random();
+    Collections.shuffle(potomstwo);
+    Collections.shuffle(mezczyzni);
+    Collections.shuffle(kobiety);
+    for(int i=0 ; (i < min(mezczyzni.size(),kobiety.size())) && (!potomstwo.isEmpty()) ; i++)
+    {
+      int los=1;
+      while(los > 0 && (!potomstwo.isEmpty()))
+      {
+        los = rand.nextInt(2);
+        mezczyzni.get(i).dodajDziecko(potomstwo.getLast());
+        kobiety.get(i).dodajDziecko(potomstwo.getLast());
+        potomstwo.pop();
+      }
+      i=i%(min(mezczyzni.size(),kobiety.size())-1);
+    }
+  }
   public static Set<Czlowiek> wypelnijDzieci(String filePath) {
     Set<Czlowiek> osoby = chooseSet();
     ArrayList<Czlowiek> dziadkowie = new ArrayList<>();
@@ -44,62 +62,23 @@ public class CzlowiekDzieciFactory {
         int wiek=Integer.parseInt(parts[2]);
         Czlowiek c = new Czlowiek(parts[0],parts[1],wiek,parts[3].equals("M")?Plec.MEZCZYZNA:Plec.KOBIETA);
         if(wiek<30)
-        {
-          if(c.getPlec()==Plec.MEZCZYZNA)
-            dzieci.add(c);
-        }
+            dzieci.push(c);
         else if(wiek<60)
         {
           sredni_wiek.push(c);
-          if(c.getPlec()==Plec.MEZCZYZNA)
-            tatusie.add(c);
-          else  mamusie.add(c);
+          (c.getPlec()==Plec.MEZCZYZNA?tatusie:mamusie).add(c);
         }
         else
-        {
-          if(c.getPlec()==Plec.MEZCZYZNA)
-            dziadkowie.add(c);
-          else  babcie.add(c);
-        }
+          (c.getPlec()==Plec.MEZCZYZNA?dziadkowie:babcie).add(c);
       }
     }
     catch (IOException e) {
       System.out.println("Błąd podczas wczytywania pliku: " + e.getMessage());
     }
-    Collections.shuffle(dziadkowie);
-    Collections.shuffle(babcie);
-    Collections.shuffle(mamusie);
-    Collections.shuffle(tatusie);
-    Collections.shuffle(sredni_wiek);
-    Collections.shuffle(dzieci);
-    Random rand = new Random();
-    for(int i=0 ; (i < min(dziadkowie.size(),babcie.size())) && (!sredni_wiek.isEmpty()) ; i++)
-    {
-      int los=1;
-      while(los > 0 && (!sredni_wiek.isEmpty()))
-      {
-        los = rand.nextInt(2);
-        dziadkowie.get(i).dodajDziecko(sredni_wiek.getLast());
-        babcie.get(i).dodajDziecko(sredni_wiek.getLast());
-        sredni_wiek.pop();
-      }
-      i=i%(min(dziadkowie.size(),babcie.size())-1);
-    }
-
-    for(int i=0 ; (i < min(tatusie.size(),mamusie.size())) && (!dzieci.isEmpty()) ; i++)
-    {
-      int los=1;
-      while(los > 0 && (!dzieci.isEmpty()))
-      {
-        los = rand.nextInt(2);
-        tatusie.get(i).dodajDziecko(dzieci.getLast());
-        mamusie.get(i).dodajDziecko(dzieci.getLast());
-        System.out.println(dzieci.pop());
-      }
-      i=i%(min(tatusie.size(),mamusie.size())-1);
-    }
-      osoby.addAll(dziadkowie);
-      osoby.addAll(babcie);
+    randomizuj(dziadkowie,babcie,sredni_wiek);
+    randomizuj(tatusie,mamusie,dzieci);
+    osoby.addAll(dziadkowie);
+    osoby.addAll(babcie);
     return osoby;
   }
 }
