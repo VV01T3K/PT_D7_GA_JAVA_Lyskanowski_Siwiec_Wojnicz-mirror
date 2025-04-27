2740de8ee601504fb22f0c5894053930101b1afd
Korneliusz Wojnicz <s198349@student.pg.edu.pl>
2025-04-28 01:10:02 +0200

    use composite key correctly

--- src/main/java/pl/edu/pg/persistance/models/Hierarchia.java
+++ src/main/java/pl/edu/pg/persistance/models/Hierarchia.java
@@ -1,21 +1,28 @@
 package pl.edu.pg.persistance.models;
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 public class Hierarchia implements IModel {
-  @Id
+  @EmbeddedId
+  private HierarchiaId id;
   @ManyToOne
+  @MapsId("przelozony")
   private Czlowiek przelozony;
-  @Id
   @ManyToOne
+  @MapsId("podwladny")
   private Czlowiek podwladny;
 
   public Hierarchia(Czlowiek przelozony, Czlowiek podwladny) {
     this.przelozony = przelozony;
     this.podwladny = podwladny;
+    this.id = new HierarchiaId(przelozony.getId(), podwladny.getId());
   }
 
   public Hierarchia() {
@@ -41,5 +48,34 @@ public class Hierarchia implements IModel {
             "przelozony=" + przelozony +
             ", podwladny=" + podwladny +
             '}';
+  }
+
+  public static class HierarchiaId implements Serializable {
+    private Integer przelozony;
+    private Integer podwladny;
+
+    public HierarchiaId() {
+    }
+
+    public HierarchiaId(Integer przelozony, Integer podwladny) {
+      this.przelozony = przelozony;
+      this.podwladny = podwladny;
+    }
+
+    // Getters & Setters
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof HierarchiaId)) return false;
+      HierarchiaId that = (HierarchiaId) o;
+      return Objects.equals(przelozony, that.przelozony) &&
+              Objects.equals(podwladny, that.podwladny);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(przelozony, podwladny);
+    }
   }
 }
